967fcd27b919d16a9cad514168a2dc4f39da1a71
Jaenil <jaenilparekh@gmail.com>
2026-01-21 01:02:23 +0530

    Revert "fixed gradient and some other issues"
    This reverts commit ce9df105d0dad81012f8e50401019881908dd5d9.
    
    Bad commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Events from "./pages/Events";
-// import { Desktop } from "./screens/Desktop";
+import { Desktop } from "./screens/Desktop";
 import React from "react";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* <Route path="/" element={<Desktop />} /> */}
-        <Route path="/events" element={<Events />} />
+        <Route path="/" element={<Desktop />} />
+        {/* <Route path="/events" element={<Events />} /> */}
       </Routes>
     </BrowserRouter>
   );
