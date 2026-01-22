6669ef51735f069266d77bf742e4454a5bf1c3b3
AadityaSharma1001 <b23me1001@iitj.ac.in>
2026-01-22 11:35:53 +0530

    Fixed Bug

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home  from "./pages/Home/Home";
 import Events from "./pages/Events/Events";
 import Profile from "./pages/Profile/Profile";
-// import { Desktop } from "./screens/Desktop";
+import { Desktop } from "./screens/Desktop";
 import React from "react";
 import SEO from "./SEO";
 
