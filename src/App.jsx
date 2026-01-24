8fbde1753187e1f4d77f18c6fdd0ee1b81389057
AadityaSharma1001 <b23me1001@iitj.ac.in>
2026-01-24 18:14:31 +0530

    Fixed

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Home from "./pages/Home/Home";
 import Events from "./pages/Events/Events";
 import Profile from "./pages/Profile/Profile";
 import Passes from "./pages/Passes/Passes";
-// import { Desktop } from "./screens/Desktop";
+import { Desktop } from "./screens/Desktop";
 import React from "react";
 import SEO from "./SEO";
 import Login from "./pages/Login/Login";
@@ -16,12 +16,12 @@ function App() {
       <BrowserRouter>
         <Navbar />
         <Routes>
-          {/* <Route path="/" element={<Desktop />} /> */}
-          <Route path="/landing" element={<Home />} />
+          <Route path="/" element={<Desktop />} />
+          {/* <Route path="/landing" element={<Home />} />
           <Route path="/events" element={<Events />} />
           <Route path="/profile" element={<Profile />} />
           <Route path="/passes" element={<Passes />} />
-          <Route path="/login" element={<Login />} />
+          <Route path="/login" element={<Login />} /> */}
         </Routes>
       </BrowserRouter>
     </>
