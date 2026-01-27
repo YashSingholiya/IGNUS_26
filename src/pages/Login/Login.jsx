920f7b17e5fec26d9fe0122fc9cadf990183237b
AadityaSharma1001 <b23me1001@iitj.ac.in>
2026-01-27 14:21:14 +0530

    Error Fixed

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import Navbar from "../../components/navbar/Navbar";
 import mascot from "./assets/mascot.svg";
